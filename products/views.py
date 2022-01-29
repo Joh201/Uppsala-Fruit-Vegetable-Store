6da3a3c5630e90a4c10c3b0b12478575ccf78055
Joh201 <johngt632@gmail.com>
2022-01-29 22:48:05 +0000

    Add filtering by category

--- products/views.py
+++ products/views.py
@@ -3,21 +3,29 @@ from django.contrib import messages
 from django.db.models import Q
 from .models import Produce
 
-
+# adopted and modified from course material
 def all_produce(request):
     ''' A view for all produce and search queries'''
 
     products = Produce.objects.all()
     query = None
+    categories = None
 
-    if 'q' in request.GET:
-        query = request.GET['q']
-        if not query:
-            messages.error(request, "Please enter a search term!")
-            return redirect(reverse('products'))
+    if request.GET:
 
-        queries = Q(name__icontains=query)
-        products = products.filter(queries)
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter a search term!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products':  products,
