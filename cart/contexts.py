91f69e232e3d7dd350f57f62e5ebfc371ee7e8e0
Joh201 <johngt632@gmail.com>
2022-01-30 21:00:41 +0000

    Add to context processor

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,13 +1,26 @@
 # adopted and modified from course material
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Produce
 
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
-    product_count = 0
+    produce_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        produce = get_object_or_404(Produce, pk=item_id)
+        total += quantity * produce.price
+        produce_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'produce': produce,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -21,7 +34,7 @@ def cart_contents(request):
     context = {
         'cart_items': cart_items,
         'total': total,
-        'product_count': product_count,
+        'produce_count': produce_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
