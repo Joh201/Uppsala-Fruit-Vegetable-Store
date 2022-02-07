d18b2c59a70432b8dcdfdb131c4d7803252e054e
Joh201 <johngt632@gmail.com>
2022-02-07 23:50:51 +0000

    Remove secret key and set debug

--- uppsala_store/settings.py
+++ uppsala_store/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k+sk59^6b)z9y8@q36w+5y3ndn-8#ot*bm7e*hbh^jg*xg&_nb'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['uppsala-store.herokuapp.com', 'localhost']
 
